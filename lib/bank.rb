e43db8d674de2330165b3b236ce2967ec297dd25
William Day <williamjday11@gmail.com>
2021-09-28 17:45:50 +0100

    Added transactions methods and tested using IRB

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,17 +1,29 @@
+require 'date'
+
 class Bank
-  attr_reader :balance
+  attr_reader :balance, :history
 
   def initialize
     @balance = 0
+    @history =[]
   end
 
   def deposit(amount)
     @balance += amount if valid?(amount)
+    @history << { "date" => Date.today, "credit" => amount, "balance" => @balance }
   end
 
   def withdraw(amount)
     if valid?(amount)
       @balance >= amount ? @balance -= amount : insufficient_funds
+      @history << { "date" => Date.today, "debit" => amount, "balance" => @balance}
+    end
+  end
+
+  def transactions
+    puts "date || credit || debit || balance"
+    @history.reverse.each do |transaction|
+      puts "#{transaction["date"]}||#{transaction["credit"]}||#{transaction["debit"]}||#{transaction["balance"]}"
     end
   end
 
